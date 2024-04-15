1cf110ee495f267c693361087dbd09f72df16466
Andrei Vladescu <52881237+AndreiVladescu@users.noreply.github.com>
2024-04-15 17:18:52 +0300

    Add files via upload

---
+++ network test.ps1
@@ -0,0 +1,5 @@
+﻿
+$link_speed = (Get-NetAdapter | Where-Object Name -eq Ethernet | Select -expand LinkSpeed).toString()
+if (!$link_speed.Contains("Gbps")){
+    Write-Output("Re-insert Ethernet Cable")
+}
